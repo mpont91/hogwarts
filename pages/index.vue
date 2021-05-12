16d6e9748b595514a83f9d195af2c258bb2278cd
Marc Pont <slayerbleast@gmail.com>
2021-05-12 14:04:31 +0200

    feat: protype flow questions answers ending

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,182 @@
 <template>
   <v-app>
     <v-container fluid class="pa-0 ma-0 background">
-      <v-row>
-        <v-col class="text-center">
-          <v-fade-transition>
-            <v-card v-show="begin" width="75vw" class="mx-auto mt-12">
-              <v-card-title class="display-2">
-                Which Hogwarts House Do You Belong In?
-              </v-card-title>
-            </v-card>
-          </v-fade-transition>
-        </v-col>
-      </v-row>
+      <v-dialog
+        v-model="dialog"
+        transition="dialog-bottom-transition"
+        persistent
+      >
+        <v-card v-if="!house && !currentQuestion">
+          <v-card-title> Are you ready?</v-card-title>
+          <v-card-actions>
+            <v-btn @click="flow"> Begin</v-btn>
+          </v-card-actions>
+        </v-card>
+        <v-card v-if="currentQuestion">
+          <v-card-title> {{ currentQuestion.text }}</v-card-title>
+          <v-card-actions>
+            <v-btn
+              v-for="(answer, index) in currentQuestion.answers"
+              :key="index"
+              @click="setAnswer(answer)"
+            >
+              {{ answer }}
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+        <v-card v-if="house">
+          <v-card-title> {{ house }}</v-card-title>
+          <v-card-actions>
+            <v-btn @click="restart"> Start again?</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-container>
   </v-app>
 </template>
 
 <script>
+const ROOT = 1
+const INTELLIGENCE = 'Intelligence'
+const BRAVERY = 'Bravery'
+const LION = 'Lion'
+const SNAKE = 'Snake'
+const SWORD = 'Sword'
+const LOCKET = 'Locket'
+const WEREWOLF = 'Werewolf'
+const BASILISK = 'Basilisk'
+const TRANSFIGURATION = 'Transfiguration'
+const POTIONS = 'POTIONS'
+const RED = 'RED'
+const GREEN = 'GREEN'
+const WITTY = 'Witty'
+const COOK = 'A good cook'
+const FIRE = 'Fire'
+const WATER = 'Water'
+const FRIAR = 'Fat friar'
+const BARON = 'Baron'
+const GRYFFINDOR = 'Gryffindor'
+const RAVENCLAW = 'Ravenclaw'
+const HUFFLEPUFF = 'Hufflepuff'
+const SLYTHERIN = 'Slytherin'
+
 export default {
   data() {
     return {
-      begin: false,
+      dialog: false,
+      currentQuestion: null,
+      lastAnswer: null,
+      house: null,
+      questions: [
+        {
+          previous: [ROOT],
+          text: 'Which is more important?',
+          answers: [INTELLIGENCE, BRAVERY],
+        },
+        {
+          previous: [INTELLIGENCE],
+          text: 'Which animal would you rather be?',
+          answers: [LION, SNAKE],
+        },
+        {
+          previous: [LION],
+          text: 'Which would you rather have?',
+          answers: [SWORD, LOCKET],
+        },
+        {
+          previous: [SWORD],
+          text: "What's your favorite magical creature?",
+          answers: [WEREWOLF, BASILISK],
+        },
+        {
+          previous: [LOCKET, RED, BASILISK],
+          text: 'Which would you rather be?',
+          answers: [WITTY, COOK],
+        },
+        {
+          previous: [SNAKE, TRANSFIGURATION, FIRE],
+          text: 'Which color do you prefer?',
+          answers: [RED, GREEN],
+        },
+        {
+          previous: [BRAVERY],
+          text: 'What would you rather learn about?',
+          answers: [TRANSFIGURATION, POTIONS],
+        },
+        {
+          previous: [POTIONS],
+          text: 'Which element is better?',
+          answers: [FIRE, WATER],
+        },
+        {
+          previous: [COOK, GREEN, WATER],
+          text: 'Who is the coolest ghost',
+          answers: [FRIAR, BARON],
+        },
+      ],
+      endings: [
+        {
+          answer: WEREWOLF,
+          house: GRYFFINDOR,
+        },
+        {
+          answer: WITTY,
+          house: RAVENCLAW,
+        },
+        {
+          answer: FRIAR,
+          house: HUFFLEPUFF,
+        },
+        {
+          answer: BARON,
+          house: SLYTHERIN,
+        },
+      ],
     }
   },
   mounted() {
-    setTimeout(() => {
-      this.begin = true
-    }, 2000)
+    this.restart()
+  },
+  methods: {
+    flow() {
+      if (!this.lastAnswer) {
+        this.setFirstQuestion()
+      } else {
+        const ending = this.endings.filter((e) => e.answer === this.lastAnswer)
+        if (ending.length === 1) {
+          this.end(ending[0].house)
+        } else {
+          this.setNextQuestion()
+        }
+      }
+    },
+    setFirstQuestion() {
+      this.currentQuestion = this.questions.filter((q) => {
+        return q.previous.includes(ROOT)
+      })[0]
+    },
+    setNextQuestion() {
+      this.currentQuestion = this.questions.filter((q) => {
+        return q.previous.includes(this.lastAnswer)
+      })[0]
+    },
+    setAnswer(answer) {
+      this.lastAnswer = answer
+      this.flow()
+    },
+    end(house) {
+      this.currentQuestion = null
+      this.house = house
+    },
+    restart() {
+      this.dialog = false
+      this.house = null
+      this.currentQuestion = null
+      this.lastAnswer = null
+      setTimeout(() => {
+        this.dialog = true
+      }, 2000)
+    },
   },
 }
 </script>
